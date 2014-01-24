6441688c41953211ea7327dad4e5f9c84ccbdb43
Thomas Maurer <thomas.wellan@gmail.com>
2014-01-24 14:31:20 +0100

    correction article visible

--- app/Controller/ArticlesController.php
+++ app/Controller/ArticlesController.php
@@ -4,14 +4,14 @@ class Articlescontroller extends AppController {
 	
 	function index() {
         $this->set('articles', $this->article->find('first',array('order'=>'id DESC')));
-		$this->set('lastarticles', $this->article->find('all',array('order'=>'id DESC Limit 5','conditions' =>array('articles.VISIBLE_ARTICLE = 1'))));
+		$this->set('lastarticles', $this->article->find('all',array('order'=>'id DESC Limit 5','conditions' =>array('article.VISIBLE_ARTICLE = 1'))));
 		$this->set('rubriques_parents', $this->article->rubrique->find('all',array('conditions' =>array('rubrique.ID_RUBRIQUE_PARENT IS NULL'))));
 		$this->set('rubriques_filles', $this->article->rubrique->find('all',array('conditions' =>array('rubrique.ID_RUBRIQUE_PARENT IS NOT NULL'))));
     }
 	
 	public function creation_article() {
 		$this->set('articles', $this->article->find('first',array('order'=>'id DESC')));
-		$this->set('lastarticles', $this->article->find('all',array('order'=>'id DESC Limit 5','conditions' =>array('articles.VISIBLE_ARTICLE = 1'))));
+		$this->set('lastarticles', $this->article->find('all',array('order'=>'id DESC Limit 5','conditions' =>array('article.VISIBLE_ARTICLE = 1'))));
 		$this->set('rubriques_parents', $this->article->rubrique->find('all',array('conditions' =>array('rubrique.ID_RUBRIQUE_PARENT IS NULL'))));
 		$this->set('rubriques_filles', $this->article->rubrique->find('all',array('conditions' =>array('rubrique.ID_RUBRIQUE_PARENT IS NOT NULL'))));
 		
