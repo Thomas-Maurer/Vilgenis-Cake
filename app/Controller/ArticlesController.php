8c7d94b06f9ce168e4335a80b4053578c21c72e6
Thomas Maurer <thomas.wellan@gmail.com>
2014-01-27 11:34:01 +0100

    changement article / création

--- app/Controller/ArticlesController.php
+++ app/Controller/ArticlesController.php
@@ -24,19 +24,29 @@ class Articlescontroller extends AppController {
              $this->article->save($this->data);
 			 $this->request->data['contient']['ID_ARTICLE']=$this->article->id; // Creation avec la rubrique
 			 //var_dump($this->data);
+			 $idrubriquemère = $this->request->data['contient']['ID_RUBRIQUE'] ;
+			 //var_dump($this->article->rubrique->find('count',array('conditions' =>array("rubrique.ID_RUBRIQUE_PARENT ='$idrubriquemère' "))));
 			 $resultat = $this->article->contient->save($this->data);
-			 $this->request->data['contient']['ID_RUBRIQUE']=$this->request->data['contient']['Sousrubrique']; // Creation avec la sous rubrique
-			 //var_dump($this->data);
-			 $this->article->contient->create();
-			 $resultat2 = $this->article->contient->save($this->data);
-			 if ($resultat and $resultat2) {
+			 if ($this->article->rubrique->find('count',array('conditions' =>array("rubrique.ID_RUBRIQUE_PARENT = '$idrubriquemère'")))> 0){
+				 $this->request->data['contient']['ID_RUBRIQUE']=$this->request->data['contient']['Sousrubrique']; // Creation avec la sous rubrique
+				 //var_dump($this->data);
+				 $this->article->contient->create();
+				 $resultat2 = $this->article->contient->save($this->data);
+			 }
+			 if ($resultat or $resultat and $resultat2) {
                 $this->flash(
             'Création d\'article terminé.',
             array(
                                 'controller'=> '',
                                 'action'=>'index')
             );
-            }
+            }else {$this->flash(
+            'Création d\'article annulée.',
+            array(
+                                'controller'=> '',
+                                'action'=>'index')
+            );
+			}
         }
     }
 	
